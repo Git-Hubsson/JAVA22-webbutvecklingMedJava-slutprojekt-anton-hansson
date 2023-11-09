c883be9233a4697b96a3d8614503aeae7cf10b51
Git-Hubsson <112413627+Git-Hubsson@users.noreply.github.com>
2023-11-09 19:01:05 +0100

    Rewrote for better cookie handling

--- slutprojekt/src/main/java/controller/OWServlet.java
+++ slutprojekt/src/main/java/controller/OWServlet.java
@@ -11,7 +11,9 @@ import model.getDateAndTime;
 import model.getTheTemperature;
 import model.weatherBean;
 import model.getTheWeather;
+
 import java.io.IOException;
+import java.net.URLEncoder;
 
 @WebServlet(name = "OWServlet", value = "/OWServlet")
 public class OWServlet extends HttpServlet {
@@ -23,11 +25,8 @@ public class OWServlet extends HttpServlet {
         String countryStr = request.getParameter("country");
 
         weatherBean wBean = new weatherBean(cityStr, countryStr);
-
         getTheWeather.getWeather(wBean);
-
         getDateAndTime.getDate(wBean);
-
         getTheTemperature.getTemperature(wBean);
 
         Cookie[] cookies = request.getCookies();
@@ -41,23 +40,23 @@ public class OWServlet extends HttpServlet {
             }
         }
 
-        if (allowCookies == true){
-        Cookie recentSearchCookie = new Cookie(countryStr, cityStr);
-        recentSearchCookie.setMaxAge(60*5);
-        response.addCookie(recentSearchCookie);
+        if (allowCookies) {
+            String cookieValue = URLEncoder.encode(cityStr + "," + countryStr, "UTF-8");
+            Cookie recentSearchCookie = new Cookie("search_" + cityStr, cookieValue);
+            recentSearchCookie.setMaxAge(60 * 5);
+            recentSearchCookie.setPath("/");
+            response.addCookie(recentSearchCookie);
         }
 
         request.setAttribute("wBean", wBean);
-
         RequestDispatcher rd = request.getRequestDispatcher("views/showWeather.jsp");
         rd.forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         Cookie allowCookiesCookie = new Cookie("allowCookies", "true");
-        allowCookiesCookie.setMaxAge(60*5);
+        allowCookiesCookie.setMaxAge(60 * 5);
         response.addCookie(allowCookiesCookie);
         response.sendRedirect("index.jsp");
     }
